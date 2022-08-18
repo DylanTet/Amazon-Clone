34a8cd7244470d5d6b9b88c7fc18b28fdeeba30d
DylanTet <100033822+DylanTet@users.noreply.github.com>
2022-08-18 20:42:44 +0000

    Finished Subtotal component adding total item cost

--- src/Context/reducer.jsx
+++ src/Context/reducer.jsx
@@ -3,6 +3,9 @@ export const initialState = {
     user: []
 }
 
+export const getBasketTotal = (basket) => basket?.reduce((amount, item) => item.price + amount, 0)
+
+
 function reducer (state, action) {
 
     switch(action.type) {
--- src/components/Subtotal/subtotal.jsx
+++ src/components/Subtotal/subtotal.jsx
@@ -1,5 +1,6 @@
 import { useStateValue } from '../../Context/context'
 import './subtotal.scss'
+import { getBasketTotal } from '../../Context/reducer'
 
 const Subtotal = () => {
 
@@ -8,7 +9,7 @@ const Subtotal = () => {
     return (
 
         <div className='subtotal'>
-            <p>Subtotal ( {basket.length} item(s) ) : $0</p>
+            <p>Subtotal ( {basket.length} item(s) ) : $ {` ${getBasketTotal(basket)} `}</p>
 
             <small className='subtotal__checkbox'>
                 <input type="checkbox" /> This order is a gift
@@ -17,7 +18,10 @@ const Subtotal = () => {
             <button>Proceed To Checkout</button>
 
         </div>
+
     )
+     
+
 }
 
 export default Subtotal
